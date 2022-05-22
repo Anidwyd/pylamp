0d6e1a0e5846cc5a04153880cf8434dfb508e230
Juju-La-Praline <julien.canitrot@gmail.com>
2022-05-22 11:39:50 +0200

    backward maxpool

--- lamp/pooling.py
+++ lamp/pooling.py
@@ -8,6 +8,7 @@ class MaxPool1D(Module):
         super().__init__()
         self.k_size = k_size
         self.stride = stride if stride else k_size
+        self.indx = list()
 
     def forward(self, X):
         batch, length, chan_in = X.shape
@@ -15,9 +16,14 @@ class MaxPool1D(Module):
 
         res = np.zeros((batch, d_out, chan_in))
 
+        # for k in range(d_out):
+        #     t1, t2 = self.stride * k, 2 * (self.k_size // 2) + k * self.stride + 1
+        #     res[:, k, :] = np.amax(X[:, t1:t2, :], axis=1)
+        
         for k in range(d_out):
             t1, t2 = self.stride * k, 2 * (self.k_size // 2) + k * self.stride + 1
-            res[:, k, :] = np.amax(X[:, t1:t2, :], axis=1)
+            self.indx = np.where(X == np.amax(X[:, t1:t2, :], axis=1))
+            res[:, k, :] = X[:,self.indx:,]
 
         return res
 
@@ -25,4 +31,13 @@ class MaxPool1D(Module):
         pass
 
     def backward_delta(self, X, delta):
-        raise NotImplementedError
+        batch,  d_out, chan_in = delta.shape
+        length = (d_out - 1) * self.stride + self.k_size
+        
+        res = np.zeros((batch, length, chan_in))
+
+        res[np.repeat(range(batch),chan_in),self.indx,list(range(chan_in))*batch]
+        
+
+
+        return res
