7c1875fcb7fe0043bdb6462aa856d92b3eadb81c
Anidwyd <jules.dubreuil@outlook.fr>
2022-05-26 04:48:46 +0200

    cleaned conv/pooling

--- lamp/conv.py
+++ lamp/conv.py
@@ -18,50 +18,25 @@ class Conv1D(Module):
     def zero_grad(self):
         self._gradient = np.zeros((self.k_size, self.chan_in, self.chan_out))
 
-    @staticmethod
-    @njit
-    def forward_nb(X, params, stride):
-        chan_out, k_size, _ = params.shape
-        batch, length, _ = X.shape
-        d_out = (length - k_size) // stride + 1
-        output = np.zeros((batch, d_out, chan_out))
-        idx_out = 0
-
-        for i in range(0, length, stride):
-            if idx_out == d_out:
-                break
-
-            for b in range(batch):
-                window = X[b, i : i + k_size, :]
-                for c in range(chan_out):
-                    kernel = params[c, :, :]
-                    output[b, idx_out, c] = np.sum(kernel * window)
-
-            idx_out += 1
-
-        return output
-
     def forward(self, X):
-        # assert X.shape[2] == self.chan_in
+        assert X.shape[2] == self.chan_in
 
-        # batch, length = X.shape[:2]
-        # self.d_out = (length - self.k_size) // self.stride + 1
+        batch, length = X.shape[:2]
+        self.d_out = (length - self.k_size) // self.stride + 1
 
-        # res = np.zeros((batch, self.d_out, self.chan_out))
+        res = np.zeros((batch, self.d_out, self.chan_out))
 
-        # for k in range(self.d_out):
-        #     t1, t2 = k * self.stride, 2 * (self.k_size // 2) + k * self.stride + 1
-        #     window = X[:, t1:t2, :, np.newaxis]
-        #     res[:, k, :] = np.sum(
-        #         window * self._parameters[np.newaxis, :, :, :], axis=(1, 2)
-        #     )
+        for k in range(self.d_out):
+            t1, t2 = k * self.stride, 2 * (self.k_size // 2) + k * self.stride + 1
+            window = X[:, t1:t2, :, np.newaxis]
+            res[:, k, :] = np.sum(
+                window * self._parameters[np.newaxis, :, :, :], axis=(1, 2)
+            )
 
-        # return res
-        # batch, length = X.shape[:2]
-        return self.forward_nb(X, self._parameters, self.stride)
+        return res
 
     def backward_update_gradient(self, X, delta):
-        res = np.zeros((self.k_size, self.chan_in, self.chan_out))
+        res = np.zeros_like(self._parameters)
 
         for k in range(self.k_size):
             t1, t2 = k * self.stride, self.d_out + k * self.stride
@@ -70,65 +45,33 @@ class Conv1D(Module):
 
         return res
 
-    @staticmethod
-    @njit
-    def backward_nb(X, params, delta, stride):
-        chan_out, k_size, _ = params.shape
-        batch, length, _ = X.shape
-        d_out = delta.shape[1]
-
-        grad_input = np.zeros_like(X)
-        grad_W = np.zeros_like(params)
-        idx_out = 0
-        for i in range(0, length, stride):
-            if idx_out == d_out:
-                break
-
-            for c in range(chan_out):
-                for b in range(batch):
-                    kernel = params[c, :, :]  # ksz, chan_in
-                    grad_input[b, i : i + k_size, :] += kernel * delta[b, idx_out, c]
-                    grad_W[c, :, :] += X[b, i : i + k_size, :] * delta[b, idx_out, c]
-
-            idx_out += 1
-
-        return grad_input, grad_W / batch
-
     def backward_delta(self, X, delta):
-        # assert X.shape[2] == self.chan_in
-        # batch = X.shape[0]
+        assert X.shape[2] == self.chan_in
+        batch = X.shape[0]
 
-        # res = np.zeros((batch, self.d_out, self.chan_in))
+        res = np.zeros((batch, self.d_out, self.chan_in))
 
-        # for k in range(self.d_out):
-        #     t1, t2 = 0, self.k_size
-        #     d1 = -k * self.stride - 2 + self.k_size
-        #     d2 = -(k * self.stride + 2)
+        for k in range(self.d_out):
+            t1, t2 = 0, self.k_size
+            d1 = -k * self.stride - 2 + self.k_size
+            d2 = -(k * self.stride + 2)
 
-        #     if k == 0:
-        #         t1 = 0
-        #         t2 = k + 1
-        #         d1 = None
-        #         d2 = -1
+            if k == 0:
+                t1, t2 = 0, k + 1
+                d1, d2 = None, -1
 
-        #     elif k <= self.k_size - 1:
-        #         t1 = 0
-        #         t2 = k + 1
-        #         d1 = -1
-        #         d2 = -k - 2
+            elif k <= self.k_size - 1:
+                t1, t2 = 0, k + 1
+                d1, d2 = -1, -k - 2
 
-        #     elif k > self.d_out - self.k_size - 1:
-        #         t1 = self.k_size - self.d_out + k
-        #         t2 = self.k_size
-        #         d1 = -k + 2
-        #         d2 = 0
+            elif k > self.d_out - self.k_size - 1:
+                t1, t2 = self.k_size - self.d_out + k, self.k_size
+                d1, d2 = -k + 2, 0
 
-        #     res[:, k, :] = np.sum(
-        #         np.flip(self._parameters, axis=1)[np.newaxis, t1:t2, :, :]
-        #         * delta[:, d2:d1, np.newaxis, :],
-        #         axis=(1, 3),
-        #     )
+            res[:, k, :] = np.sum(
+                np.flip(self._parameters, axis=1)[np.newaxis, t1:t2, :, :]
+                * delta[:, d2:d1, np.newaxis, :],
+                axis=(1, 3),
+            )
 
-        # return res
-        # output,
-        return self.backward_nb(X, self._parameters, delta, self.stride)
+        return res
--- lamp/pooling.py
+++ lamp/pooling.py
@@ -11,80 +11,38 @@ class MaxPool1D(Module):
         self.k_size = k_size
         self.stride = stride if stride else k_size
 
-    @staticmethod
-    @njit
-    def forward_nb(X, k_size, stride):
-        batch, length, chan_in = X.shape
-        d_out = int(np.floor((length - k_size) / stride)) + 1
-
-        output = np.zeros((batch, d_out, chan_in))
-        saved_argmax = np.zeros_like(output, dtype=np.int32)
-
-        idx_out = 0
-        for i in range(0, length, stride):
-            if idx_out == d_out:
-                break
-            for b in range(batch):
-                for c in range(chan_in):
-                    window = X[b, i : i + k_size, c]
-                    output[b, idx_out, c] = np.max(window)
-                    saved_argmax[b, idx_out, c] = np.argmax(window)
-            idx_out += 1
-
-        return output, saved_argmax
-
     def forward(self, X):
-        # batch, length, chan_in = X.shape
-        # self.d_out = (length - self.k_size) // self.stride + 1
+        batch, length, chan_in = X.shape
+        self.d_out = (length - self.k_size) // self.stride + 1
 
-        # res = np.zeros((batch, self.d_out, chan_in))
+        res = np.zeros((batch, self.d_out, chan_in))
 
-        # self.mask = np.zeros((batch, chan_in, length))
+        self.mask = np.zeros((batch, chan_in, length))
 
-        # for k in range(self.d_out):
-        #     t1, t2 = self.stride * k, 2 * (self.k_size // 2) + k * self.stride + 1
-        #     window = X[:, t1:t2, :]
-        #     amax = np.amax(window, axis=1)
-        #     res[:, k, :] = amax
+        for k in range(self.d_out):
+            t1, t2 = self.stride * k, 2 * (self.k_size // 2) + k * self.stride + 1
+            window = X[:, t1:t2, :]
+            amax = np.amax(window, axis=1)
+            res[:, k, :] = amax
 
-        #     # saving max indices
-        #     idx = np.where(X.transpose(0, 2, 1) == amax.reshape(batch, chan_in, 1))
-        #     self.mask[idx] += 1
+            # saving max indices
+            idx = np.where(X.transpose(0, 2, 1) == amax.reshape(batch, chan_in, 1))
+            self.mask[idx] += 1
 
-        # self.mask = self.mask.transpose(0, 2, 1)
+        self.mask = self.mask.transpose(0, 2, 1)
 
-        # return res
-
-        output, self.saved_argmax = self.forward_nb(X, self.k_size, self.stride)
-        return output
+        return res
 
     def backward_update_gradient(self, X, delta):
         pass
 
-    @staticmethod
-    @njit
-    def backward_nb(delta, length, stride, saved_argmax):
-        batch_, d_out, chan_in = delta.shape
-        grad_input = np.zeros((batch_, length, chan_in))
-
-        for b in range(batch_):
-            for c in range(chan_in):
-                for i in range(d_out):
-                    grad_input[b, i * stride + saved_argmax[b, i, c], c] = delta[
-                        b, i, c
-                    ]
-        return grad_input
-
     def backward_delta(self, X, delta):
-        # size_diff = X.shape[1] - delta.shape[1]
+        size_diff = X.shape[1] - delta.shape[1]
 
-        # d, r = size_diff // 2, size_diff % 2
+        d, r = size_diff // 2, size_diff % 2
 
-        # delta = np.pad(
-        #     delta, ((0, 0), (d, d + r), (0, 0)), "constant", constant_values=(0, 0)
-        # )
+        delta = np.pad(
+            delta, ((0, 0), (d, d + r), (0, 0)), "constant", constant_values=(0, 0)
+        )
 
-        # return self.mask * delta
-
-        length = X.shape[1]
-        return self.backward_nb(delta, length, self.stride, self.saved_argmax)
+        return self.mask * delta
